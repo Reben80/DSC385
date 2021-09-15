6891c6744fcbe1f5dab5615efe6fbbd48844eefb
Claus Wilke <wilke@austin.utexas.edu>
2021-09-14 22:58:05 -0500

    new build

--- _deploy.sh
+++ _deploy.sh
@@ -4,5 +4,6 @@ Rscript -e "rmarkdown::render_site(encoding = 'UTF-8')"
 rm -rf ./docs/slides/*.Rmd
 rm -rf ./docs/worksheets/*.html
 rm -rf ./docs/datasets/*.R
+touch ./docs/.nojekyll
 git add ./docs/
 
